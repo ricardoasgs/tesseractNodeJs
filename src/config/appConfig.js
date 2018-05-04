e06ea90c26cb8960689efe745c3776dd7a9bd8c1
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2018-05-04 11:46:50 -0300

    Ajustes

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-const port = '3001';
+const port = process.env.PORT || '3001';
 
 const app = express();
 
